6f7e897cef01e65be587496bece610ff73111453
gyzerok <gyzerok@gmail.com>
2013-12-13 21:40:06 +0700

    Crad and Hand TOString added

--- LuckyStrike/Common/Domain/Card.cs
+++ LuckyStrike/Common/Domain/Card.cs
@@ -16,5 +16,106 @@ namespace Common.Domain
             this.Value = value;
             this.Suit = suit;
         }
+
+        public override string ToString()
+        {
+            switch (this.Value)
+            {
+                case Value._2:
+                    return "2";
+                case Value._3:
+                    return "3";
+                case Value._4:
+                    return "4";
+                case Value._5:
+                    return "5";
+                case Value._6:
+                    return "6";
+                case Value._7:
+                    return "7";
+                case Value._8:
+                    return "8";
+                case Value._9:
+                    return "9";
+                case Value._T:
+                    return "T";
+                case Value._J:
+                    return "J";
+                case Value._Q:
+                    return "Q";
+                case Value._K:
+                    return "K";
+                case Value._A:
+                    return "A";
+                default:
+                    return "";
+            }
+        }
+
+        public static Card FromString(string str)
+        {
+            var card = new Card();
+
+            switch (str[0])
+            {
+                case '2':
+                    card.Value = CardValue._2;
+                    break;
+                case '3':
+                    card.Value = CardValue._3;
+                    break;
+                case '4':
+                    card.Value = CardValue._4;
+                    break;
+                case '5':
+                    card.Value = CardValue._5;
+                    break;
+                case '6':
+                    card.Value = CardValue._6;
+                    break;
+                case '7':
+                    card.Value = CardValue._7;
+                    break;
+                case '8':
+                    card.Value = CardValue._8;
+                    break;
+                case '9':
+                    card.Value = CardValue._9;
+                    break;
+                case 'T':
+                    card.Value = CardValue._T;
+                    break;
+                case 'J':
+                    card.Value = CardValue._J;
+                    break;
+                case 'Q':
+                    card.Value = CardValue._Q;
+                    break;
+                case 'K':
+                    card.Value = CardValue._K;
+                    break;
+                default:
+                    card.Value = CardValue._A;
+                    break;
+            }
+
+            switch (str[1])
+            {
+                case 's':
+                    card.Suit = CardSuit.S;
+                    break;
+                case 'c':
+                    card.Suit = CardSuit.C;
+                    break;
+                case 'd':
+                    card.Suit = CardSuit.D;
+                    break;
+                default:
+                    card.Suit = CardSuit.H;
+                    break;
+            }
+
+            return card;
+        }
     }
 }
--- LuckyStrike/Common/Domain/Hand.cs
+++ LuckyStrike/Common/Domain/Hand.cs
@@ -9,5 +9,26 @@ namespace Common.Domain
     public class Hand
     {
         public List<Card> Cards { get; private set; }
+
+        public override string ToString()
+        {
+            string ret = "";
+
+            if ((int)Cards[0].Value < (int)Cards[1].Value)
+            {
+                ret = this.Cards[1].ToString() + this.Cards[0].ToString();
+            }
+            else
+            {
+                ret = this.Cards[0].ToString() + this.Cards[1].ToString();
+            }
+
+            if (this.Cards[0].Suit == this.Cards[1].Suit)
+                ret += "s";
+            else
+                ret += "o";
+
+            return ret;
+        }
     }
 }
