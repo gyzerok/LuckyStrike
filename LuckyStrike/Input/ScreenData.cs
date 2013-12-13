408498cd1f1f585a5366d41cfbd8ffe89f95c351
zuiris <siriuz1g@gmail.com>
2013-12-13 20:55:13 +0600

    grubber implemented

--- LuckyStrike/Input/ScreenData.cs
+++ LuckyStrike/Input/ScreenData.cs
@@ -11,9 +11,37 @@ namespace Input
 {
     public class ScreenData : AbstractData
     {
-        public ScreenData()
+        private List<Bitmap> handsBitmaps;
+        private List<Bitmap> cardsBitmaps;
+        private List<Bitmap> dealersBitmaps;
+        private List<Bitmap> betsBitmaps;
+ 
+        public ScreenData(List<Bitmap> hands, List<Bitmap> cards, List<Bitmap> dealers, List<Bitmap> bets)
         {
-            
+            this.handsBitmaps = hands;
+            this.cardsBitmaps = cards;
+            this.dealersBitmaps = dealers;
+            this.betsBitmaps = bets;
+        }
+
+        public List<Bitmap> GetHandsBitmaps()
+        {
+            return this.handsBitmaps;
+        }
+
+        public List<Bitmap> GetCardsBitmaps()
+        {
+            return this.cardsBitmaps;
+        }
+
+        public List<Bitmap> GetDealersBitmaps()
+        {
+            return this.dealersBitmaps;
+        }
+
+        public List<Bitmap> GetBetssBitmaps()
+        {
+            return this.betsBitmaps;
         }
 
         public override void LoadConfig()
