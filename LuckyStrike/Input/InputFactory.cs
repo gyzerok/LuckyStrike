4269e3931fb100fbd9e2317ad85dfbc35fb92342
gyzerok <gyzerok@gmail.com>
2013-12-13 18:39:03 +0700

    Input project and class structure added

---
+++ LuckyStrike/Input/InputFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input
+{
+    public abstract class InputFactory
+    {
+        public abstract AbstractGrubber CreateGrubber();
+        public abstract AbstractInterpreter CreateInterpreter();
+    }
+}
---
+++ LuckyStrike/Input/ScreenFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input
+{
+    public class ScreenFactory : InputFactory
+    {
+        public override AbstractGrubber CreateGrubber()
+        {
+            return new ScreenGrubber();
+        }
+
+        public override AbstractInterpreter CreateInterpreter()
+        {
+            return new ScreenInterpreter();
+        }
+    }
+}
