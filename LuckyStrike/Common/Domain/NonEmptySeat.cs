bcba495148f4c7a9c2161623d1d617667e2f333c
gyzerok <gyzerok@gmail.com>
2013-12-19 19:36:58 +0700

    minor fixes

--- LuckyStrike/Common/Domain/NonEmptySeat.cs
+++ LuckyStrike/Common/Domain/NonEmptySeat.cs
@@ -6,6 +6,34 @@
         public Hand Hand { get; set; }
         public Activity Activity { get; private set; }
 
+        public NonEmptySeat LeftActive
+        {
+            get
+            {
+                AbstractSeat checkingSeat = this.Left;
+                while (checkingSeat is EmptySeat || (checkingSeat as NonEmptySeat).Activity.Decision != Decision.FOLD)
+                {
+                    checkingSeat = checkingSeat.Left;
+                }
+
+                return (NonEmptySeat) checkingSeat;
+            }
+        }
+
+        public NonEmptySeat RightActive
+        {
+            get
+            {
+                AbstractSeat checkingSeat = this.Right;
+                while (checkingSeat is EmptySeat || (checkingSeat as NonEmptySeat).Activity.Decision != Decision.FOLD)
+                {
+                    checkingSeat = checkingSeat.Right;
+                }
+
+                return (NonEmptySeat)checkingSeat;
+            }
+        }
+
         public NonEmptySeat LeftNonEmpty
         {
             get
